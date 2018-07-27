31dcb7c1f66062e12c129170c254da1de40e950c
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-27 10:27:53 +0530

    Update sam.java

--- sam.java
+++ sam.java
@@ -1,1 +1,21 @@
-ddytidbuytdtkjy
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int number;
+Scanner sc=new Scanner(System.in);
+number=sc.nextInt();
+if(number>0)
+{
+	System.out.println("positive number");
+}
+else if(number<0)
+{
+	System.out.println("negative number");
+	}
+	else
+	{
+		System.out.println("number is zero");
+  }
+  }
+}
